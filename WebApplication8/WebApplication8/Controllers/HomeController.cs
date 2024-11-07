67f085e7e8ad0515f423a7664b6a5c8d87ff2e70
PonomarenkoDM <159808442+PonomarenkoDM@users.noreply.github.com>
2024-11-07 02:16:15 +0200

    Add files via upload

---
+++ WebApplication8/WebApplication8/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication8.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("/PostUser")]
+        public IActionResult PostUser()
+        {
+            return View();
+        }
+    }
+}
